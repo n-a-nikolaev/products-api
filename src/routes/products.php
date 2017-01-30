ab61c75a8b5f5df3e742dcbf0ba6f53b5687779a
Nikolay Nikolaev <nikolaev.a.nikolay@gmail.com>
2017-01-31 01:04:40 +0200

    Add search route

--- src/routes/products.php
+++ src/routes/products.php
@@ -5,7 +5,6 @@ header("Access-Control-Allow-Origin: *");
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-
 $app->get('/products', function (Request $request, Response $response) {
     $db = new DB();
     $db->connect();
@@ -18,18 +17,35 @@ $app->get('/products', function (Request $request, Response $response) {
 
 $app->get('/products/{id}', function (Request $request, Response $response) {
     $id = $request->getAttribute('id');
-   
+
     $db = new DB();
     $db->connect();
 
     $query = 'SELECT * FROM `products` WHERE id=' . $id;
     $result = $db->select($query);
-    
+
     Helper::process_results($result);
 });
 
 $app->get('/products/group/{id}', function (Request $request, Response $response) {
     $id = $request->getAttribute('id');
+
+    $db = new DB();
+    $db->connect();
+
+    $query = 'SELECT * FROM `products` WHERE groupId=' . $id;
+    $result = $db->select($query);
+
+    Helper::process_results($result);
+});
+
+$app->get('/search', function (Request $request, Response $response) {
+    $params = $request->getQueryParams();
+    
+    $title = isset($params['title']) || '';
+    $groupId = isset($params['groupId']) || '';
+    
+    die();
     
     $db = new DB();
     $db->connect();
